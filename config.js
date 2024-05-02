bb82c573a8d874539856cf47b4761cb7710ce462
Vishal Yadav <vishalyadav882004@gmail.com>
2024-05-02 19:25:59 +0530

    Initial Commit

--- config.js
+++ config.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
-const connect = mongoose.connect("mongodb://localhost:27017/Login-tut");
-
+require('dotenv').config();
+// const connect = mongoose.connect("mongodb://localhost:27017/Login-tut");
+// const connect = mongoose.connect("mongodb+srv://user:user@cluster0.tyzugcr.mongodb.net/Login-tut?retryWrites=true&w=majority&appName=Cluster0");
+const connect = mongoose.connect(process.env.MONGO_URL);
 // Check database connected or not
 connect.then(() => {
     console.log("Database Connected Successfully");
--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const path = require("path");
 const collection = require("./config");
 const bcrypt = require('bcrypt');
+
 const session = require("express-session");
 
 const app = express();
